d0e0c7bb9545206e1aa7c50c6a4725939e57a092
thedipankar <81231654+thedipankar@users.noreply.github.com>
2024-07-29 16:33:17 +0530

    Complete Task 1

---
+++ Week_1/Day_1/task_1.sql
@@ -0,0 +1,14 @@
+CREATE DATABASE school;
+
+USE school;
+
+CREATE TABLE students (
+    s_id INT PRIMARY KEY,
+    s_name VARCHAR(100),
+    s_age INT
+);
+
+INSERT INTO students (s_id, s_name, s_age) VALUES (1, 'Ram', 21);
+INSERT INTO students (s_id, s_name, s_age) VALUES (2, 'Shamam', 21);
+
+SELECT * FROM students;
